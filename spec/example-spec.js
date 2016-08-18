a4909f5465b6d6c10b29e12a8b91b7c36bbd53ae
Arunas Skirius <arukomp@gmail.com>
2016-08-18 14:48:38 +0100

    Added a beforeEach(cb) function to use within describe blocks. See example-spec.js for an example

--- spec/example-spec.js
+++ spec/example-spec.js
@@ -1,13 +1,23 @@
 describe("Simple Tests", function(){
 
+  beforeEach(function() {
+    console.log("This block gets run before each test within this describe");
+  });
+
   describe("true or false", function() {
 
+    var someBoolean;
+
+    beforeEach(function() {
+      someBoolean = true;
+    });
+
     it("true is true", function(){
-      assert.isTrue(true);
+      assert.isTrue(someBoolean);
     });
 
     it("true is not false", function(){
-      assert.isFalse(true);
+      assert.isFalse(someBoolean);
     });
 
   });
